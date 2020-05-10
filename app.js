6099abcb49536973f178b1f22a3fa0eb9a4e8b1b
desmond <desmond@greywether.com>
2020-05-10 11:34:03 -0700

    Configure express to server reactjs-based client web-app

--- app.js
+++ app.js
@@ -11,10 +11,17 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
+// serve api endpoints
 app.use('/api/health', healthRouter);
 app.use('/api/version', versionRouter);
 app.use('/api/ping-pong', pingPongRouter);
+
+// serve the React web app from the build folder of its project dir
+app.use(express.static(path.join(__dirname, '../play-x-play-web/build')));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, '../play-x-play-web/build/index.html'));
+});
+
 
 module.exports = app;
